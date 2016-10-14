bd6a58edda35547f5aee4b7cd7a9fbfd5daae5f9
Nick Miller <nick.miller@onyxpoint.com>
2016-10-14 11:18:07 -0400

    (SIMP-1698) compliance_mapper should use puppet_vardir
    Puppet[:vardir] causes issues with Puppet 4, and sometimes the
    compliance report can't be put down. This update changes how $vardir is
    discovered to use the `puppet_vardir` fact from stblib.
    
    SIMP-1698 #close
    SIMP-1450 #comment updated compliance_mapper
    
    Change-Id: I6a443a7c8f75464112d94c18d920324592b12010

--- lib/puppet/parser/functions/compliance_map.rb
+++ lib/puppet/parser/functions/compliance_map.rb
@@ -16,42 +16,42 @@ module Puppet::Parser::Functions
             super
             @keys = []
           end
-    
+
           def self.[](*args)
             ordered_hash = new
-    
+
             if (args.length == 1 && args.first.is_a?(Array))
               args.first.each do |key_value_pair|
                 next unless (key_value_pair.is_a?(Array))
                 ordered_hash[key_value_pair[0]] = key_value_pair[1]
               end
-    
+
               return ordered_hash
             end
-    
+
             unless (args.size % 2 == 0)
               raise ArgumentError.new("odd number of arguments for Hash")
             end
-    
+
             args.each_with_index do |val, ind|
               next if (ind % 2 != 0)
               ordered_hash[val] = args[ind + 1]
             end
-    
+
             ordered_hash
           end
-    
+
           def initialize_copy(other)
             super
             # make a deep copy of keys
             @keys = other.keys
           end
-    
+
           def []=(key, value)
             @keys << key if !has_key?(key)
             super
           end
-    
+
           def delete(key)
             if has_key? key
               index = @keys.index(key)
@@ -59,80 +59,80 @@ module Puppet::Parser::Functions
             end
             super
           end
-          
+
           def delete_if
             super
             sync_keys!
             self
           end
-    
+
           def reject!
             super
             sync_keys!
             self
           end
-    
+
           def reject(&block)
             dup.reject!(&block)
           end
-    
+
           def keys
             @keys.dup
           end
-    
+
           def values
             @keys.collect { |key| self[key] }
           end
-    
+
           def to_hash
             self
           end
-    
+
           def to_a
             @keys.map { |key| [ key, self[key] ] }
           end
-    
+
           def each_key
             @keys.each { |key| yield key }
           end
-    
+
           def each_value
             @keys.each { |key| yield self[key]}
           end
-    
+
           def each
             @keys.each {|key| yield [key, self[key]]}
           end
-    
+
           alias_method :each_pair, :each
-    
+
           def clear
             super
             @keys.clear
             self
           end
-    
+
           def shift
             k = @keys.first
             v = delete(k)
             [k, v]
           end
-    
+
           def merge!(other_hash)
             other_hash.each {|k,v| self[k] = v }
             self
           end
-    
+
           def merge(other_hash)
             dup.merge!(other_hash)
           end
-    
+
           def inspect
             "#<OrderedHash #{super}>"
           end
-    
+
         private
-    
+
           def sync_keys!
             @keys.delete_if {|k| !has_key?(k)}
           end
@@ -377,7 +377,11 @@ module Puppet::Parser::Functions
 
     # This will be useful for a future iteration of the software.
     #if generate_report
-      compliance_report_target = %(#{Puppet[:vardir]}/compliance_report.yaml)
+      if Facter.value(:puppet_vardir).nil?
+        raise Puppet::ParseError, "compliance_map(): Cannot find fact `puppet_vardir`. Ensure `puppetlabs/stdlib` is installed"
+      else
+        compliance_report_target = %(#{Facter.value(:puppet_vardir)}/compliance_report.yaml)
+      end
 
       # Retrieve the catalog resource if it already exists, create one if it
       # does not
